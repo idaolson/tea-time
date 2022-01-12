e582509193994ccbf6b2b0dd163e9040007da739
Ida Olson <idaolson@gmail.com>
2022-01-12 00:26:38 -0700

    Limit routes to only those needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      patch '/subscriptions', to: 'subscriptions#update'
       resources :subscriptions, only: [:create, :update] 
-      resources :customers, only: :show do 
+      resources :customers, only: [:show] do 
         resources :subscriptions, only: :index 
-      end 
+      end
     end 
   end 
 end
