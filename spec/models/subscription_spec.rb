3455ef2f784e44b4d6c96ebf57a8b7755763c5f8
Ida Olson <idaolson@gmail.com>
2022-01-11 21:56:36 -0700

    Add validation tests for subscription model

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Subscription, type: :model do
     it { should belong_to(:customer) }
     it { should belong_to(:tea) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:customer_id) }
+    it { should validate_presence_of(:tea_id) }
+  end
 end 
