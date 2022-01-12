dd90f9c9025d00a845653d38453a62db73fb44a7
Ida Olson <idaolson@gmail.com>
2022-01-11 21:52:34 -0700

    Add validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,5 +2,7 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
 
+  validates :title, :price, :status, :frequency, :quantity, :customer_id, :tea_id, presence: true
+
   enum status: [:active, :cancelled]
 end
