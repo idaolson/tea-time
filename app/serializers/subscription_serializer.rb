fb82c9752316bc9c92d55478d201a05d8671c5c4
Ida Olson <idaolson@gmail.com>
2022-01-14 09:54:15 -0700

    Remove extra comments

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -2,6 +2,4 @@ class SubscriptionSerializer
   include JSONAPI::Serializer
 
   attributes :title, :price, :status, :frequency, :quantity, :tea_id
-
-  #change tea id to tea title if you have time
 end
